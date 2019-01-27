12b36c74fd6fd3f6d25c34a058188e44e6f53a17
Nikita Kazantsev <kaznik2013@gmail.com>
2019-01-27 22:55:26 +0300

    Update settings.py

--- SimpleVotations/settings.py
+++ SimpleVotations/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '&s6@okpozpvw0rba$$st&%zv@k3s*w=t-)^s=r8j%u+h4+!9ic'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
