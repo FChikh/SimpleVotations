a381549e771355159b74a66b3d5b58d294b4c882
Nikita Kazantsev <kaznik2013@gmail.com>
2019-01-27 23:18:21 +0300

    voting db

--- votation/models.py
+++ votation/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class VotingsBase(models.Model):
+  id = models.IntegerField()
+  author = models.CharField(max_length=20)
+  question = models.CharField(max_length=144)
+  options = models.IntegerField()
+  option1 = models.CharField(max_length=50)
+  option2 = models.CharField(max_length=50)
+  option3 = models.CharField(max_length=50)
+  option4 = models.CharField(max_length=50)
+  date = models.DateTimeField()
