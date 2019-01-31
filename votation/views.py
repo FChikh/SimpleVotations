c410c0d0acaf3475620898a786341d96ac0b2da7
Igor Nurullaev <mighty.igor@yandex.ru>
2019-01-31 21:16:43 +0300

    Куча всего короче

--- votation/views.py
+++ votation/views.py
@@ -2,12 +2,48 @@ from django.shortcuts import render
 
 
 def main(request):
-    return render(request, "main.html")
+    data = {}
+    data["votes"] = [
+        [
+            {
+                "title": "Собачки",
+                "percentage": 10
+            },
+            {
+                "title": "Котики",
+                "percentage": 20
+            },
+            {
+                "title": "Единая Россия",
+                "percentage": 70
+            }
+        ]
+    ]
+    # Хардкод
+    data["votes"] *= 10
+    return render(request, "main.html", data)
 
 
 def profile(request):
     data = {}
     data['name'] = "Игорь"
     data['surname'] = "Нуруллаев"
-    data['pic'] = "static/votation/anon.png"
+    data["votes_history"] = [
+        [
+            {
+                "title": "Собачки",
+                "percentage": 10
+            },
+            {
+                "title": "Котики",
+                "percentage": 20
+            },
+            {
+                "title": "Единая Россия",
+                "percentage": 70
+            }
+        ]
+    ]
+    # Хардкод
+    data["votes_history"] *= 10
     return render(request, "profile.html", data)
